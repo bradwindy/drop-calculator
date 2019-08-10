022eff7da03eb7975b6cf5fc75d5b121cc1b6871
Bradley Windybank <trykryda@gmail.com>
2019-08-10 16:51:27 +1200

    Minor updates

--- src/Main.jsx
+++ src/Main.jsx
@@ -48,8 +48,8 @@ export default class Main extends React.Component {
     const { timerTime } = this.state;
     let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
     let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
-    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
-    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+    // let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    // let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
 
     return (
       <div>
